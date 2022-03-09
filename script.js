9c8ffc07a75cef555c0591e02d4b09e40b5520c3
xiao2202 <46682681+xiao2202@users.noreply.github.com>
2022-03-09 04:20:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -78,6 +78,16 @@ function connectToBle() {
   }
 }
 
+//floor round a number down to an integer
+//math.random gives a random number between 0 and 1
+
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+
+}
+
+
 // write to a characteristic:
 function writeToCharacteristic(event) {
   // LED state is whether or not the button is checked:
@@ -93,13 +103,58 @@ function writeToCharacteristic(event) {
   //reply a message after press the button
   var div = document.getElementById("reply");
   //set reply field to empty after press the button (NOT WORKING)
-  document.getElementById("reply").value = "";
+  //document.getElementById("reply").value = "";
 
-  var delayInMilliseconds = 0; //10 second
+  //div.innerHTML += "<br>" +  textInput;
+  //console.log(textInput);
+
+  var delayInMilliseconds = 21000; //22 second
+
+  let response = ["Hello from Earth!",
+                  "Wanna get Starbucks later?",
+                  "Sending you a virtual hug. I hope you can feel it.",
+                  "Our song just came on the radio. Miss you!",
+                  "Just wanted to say hello.",
+                  "I wish you were here. Everybody else sucks.",
+                  "Reading your text just made my day.",
+                  "You are the best.",
+                  "You are the best friend I’ve ever had.", 
+                  "I will see you tonight at eight.",
+                  "It is not so easy as you think.",
+                  "We have got bigger problems to deal with.",
+                  "I am crying.",
+                  "Toothpaste will help keep your teeth clean.",
+                  "I like veggies too.",
+                  "I do love my new pets.",
+                  "Spring is the prettiest season.",
+                  "They do not go to school tomorrow.",
+                  "Come on down!",
+                  "Remember to eat on time!",
+                  "How is your life out there?",
+                  "I have been waiting for you for really long time!",
+                  "You are not alone!",
+                  "What is your name??",
+                  "Do you see the stars?",
+                  "I heard you graduated. Congrats!",
+                  "Hey! Just wondering what your life is like these days. I’d love to catch up.",
+                  "How has it been three months already!?",
+                  "Things are looking stressful back at home. Just wanted to make sure you're OK.",
+                  "I totally ghosted you and I’m sorry for that... ",
+                  "An alien abducted me but now I'm back. How are you?",
+                  "Could we talk or catch up soon?",
+                  "Remember when we pulled that all-nighter in college?",
+                  "Ugh, I'm so bad at responding",
+                  "Happy birthday! Let's not let another year go by without talking.",
+                  "“Want to meet up for my birthday this weekend?",
+                  "Can you believe it’s been 10 years since we landed at Proxima Centauri b?",
+                  "Wow, things are overwhelming right now, how’s your stress level these days?",
+
+                 ];
 
   //delay the message that the user sent
   setTimeout(function() {
-    div.innerHTML = "Hello from Earth";
+  //+= append message next to each other
+    div.innerHTML += "<br>"+ response[getRandomInt(response.length)] + "<br>";
   }, delayInMilliseconds);
   
   // if you're connected, write to the peripheral:
